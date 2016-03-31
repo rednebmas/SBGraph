6f6e530ef10c7279a84052de7ae1d3092a322470
Sam Bender <rednebmas@gmail.com>
2016-03-31 14:27:36 -0700

    Basic setup

---
+++ SBGraph/SBGraphView.h
@@ -0,0 +1,13 @@
+//
+//  SBGraphView.h
+//  SBGraph
+//
+//  Created by Sam Bender on 3/31/16.
+//  Copyright © 2016 Sam Bender. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBGraphView : UIView
+
+@end
---
+++ SBGraph/SBGraphView.m
@@ -0,0 +1,21 @@
+//
+//  SBGraphView.m
+//  SBGraph
+//
+//  Created by Sam Bender on 3/31/16.
+//  Copyright © 2016 Sam Bender. All rights reserved.
+//
+
+#import "SBGraphView.h"
+
+@implementation SBGraphView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
