72f18fd6e3085e1224a99f9ca5decb56092c8cec
Sam Bender <rednebmas@gmail.com>
2016-03-31 14:17:19 -0700

    Initial Commit

---
+++ SBGraph/SBGraph.h
@@ -0,0 +1,19 @@
+//
+//  SBGraph.h
+//  SBGraph
+//
+//  Created by Sam Bender on 3/31/16.
+//  Copyright © 2016 Sam Bender. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBGraph.
+FOUNDATION_EXPORT double SBGraphVersionNumber;
+
+//! Project version string for SBGraph.
+FOUNDATION_EXPORT const unsigned char SBGraphVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBGraph/PublicHeader.h>
+
+
